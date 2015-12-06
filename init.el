fd307f52da49fb3df3ae33a7747bb8b29325fa0e
Peter Czibik <p.czibik@gmail.com>
2015-12-06 22:27:57 +0100

    initial commit

---
+++ init.el
@@ -0,0 +1,42 @@
+(require 'package)
+
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+(defun ensure-package-installed (&rest packages)
+  "Assure every package is installed, ask for installation if it’s not.
+
+Return a list of installed packages or nil for every skipped package."
+  (mapcar
+   (lambda (package)
+     (if (package-installed-p package)
+         nil
+       (if (y-or-n-p (format "Package %s is missing. Install it? " package))
+           (package-install package)
+         package)))
+   packages))
+
+;; Make sure to have downloaded archive description.
+(or (file-exists-p package-user-dir)
+    (package-refresh-contents))
+
+;; Activate installed packages
+(package-initialize)
+
+;; Packages installed
+(ensure-package-installed
+ 'helm
+ 'projectile
+ 'flycheck
+ 'magit)
+
+;; Set helm as default M-x provider
+(helm-mode 1)
+
+;; Disable backup and autosave files
+(setq make-backup-files nil)
+(setq auto-save-default nil)
