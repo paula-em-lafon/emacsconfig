00a854eb76d72df56bc1befe7f82d5a78b0cfdc3
Peter Czibik <p.czibik@gmail.com>
2015-12-15 17:26:46 +0100

    Finally managed to make company work

--- init.el
+++ init.el
@@ -29,18 +29,18 @@
    (global-company-mode 1)
    (global-linum-mode 1)))
 
-(eval-after-load
-    'company-mode
-  (lambda ()
-    ;; Slightly better autocomplete on tab
-    (define-key company-active-map [tab] 'company-complete-common-or-cycle)
-    (define-key company-active-map (kbd "TAB") 'company-complete-common-or-cycle)))
+(add-hook
+ 'company-mode-hook
+ (lambda ()
+   ;; Slightly better autocomplete on tab
+   (define-key company-active-map [tab] 'company-complete-common-or-cycle)
+   (define-key company-active-map (kbd "TAB") 'company-complete-common-or-cycle)))
 
 (setq-default
  company-idle-delay 0.1)
 
 ;; Transparent emacs yay!
-(set-frame-parameter (selected-frame) 'alpha '(98 95))
+(set-frame-parameter (selected-frame) 'alpha '(98))
 
 ;; (load-theme 'tronesque)
 ;; (tronesque-mode-line)
@@ -74,7 +74,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("4f2ede02b3324c2f788f4e0bad77f7ebc1874eff7971d2a2c9b9724a50fb3f65" "870a63a25a2756074e53e5ee28f3f890332ddc21f9e87d583c5387285e882099" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "cedd3b4295ac0a41ef48376e16b4745c25fa8e7b4f706173083f16d5792bb379" default))))
+    ("0aa12caf6127772c1a38f7966de8258e7a0651fb6f7220d0bbb3a0232fba967f" "4f2ede02b3324c2f788f4e0bad77f7ebc1874eff7971d2a2c9b9724a50fb3f65" "870a63a25a2756074e53e5ee28f3f890332ddc21f9e87d583c5387285e882099" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "cedd3b4295ac0a41ef48376e16b4745c25fa8e7b4f706173083f16d5792bb379" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
