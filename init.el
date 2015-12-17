16393d0f31218f837e39ded47582cb4a7673f3f7
Peter Czibik <p.czibik@gmail.com>
2015-12-17 13:12:12 +0100

    Add saving on focus lose function.

--- init.el
+++ init.el
@@ -21,6 +21,15 @@
  (lambda ()
    (setenv "TERM" "dumb")))
 
+
+(defun my-save-all ()
+  (interactive)
+  (save-some-buffers t))
+
+(add-hook
+ 'focus-out-hook
+ 'my-save-all)
+
 ;; Enable which-key mode globally
 (add-hook
  'after-init-hook
@@ -53,6 +62,7 @@
 
 ;; Disable backup and autosave files
 (setq-default
+ inhibit-startup-screen t
  indent-tabs-mode nil
  auto-save-default nil
  make-backup-files nil)
@@ -74,7 +84,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("0aa12caf6127772c1a38f7966de8258e7a0651fb6f7220d0bbb3a0232fba967f" "4f2ede02b3324c2f788f4e0bad77f7ebc1874eff7971d2a2c9b9724a50fb3f65" "870a63a25a2756074e53e5ee28f3f890332ddc21f9e87d583c5387285e882099" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "cedd3b4295ac0a41ef48376e16b4745c25fa8e7b4f706173083f16d5792bb379" default))))
+    ("03e3e79fb2b344e41a7df897818b7969ca51a15a67dc0c30ebbdeb9ea2cd4492" "cdfb22711f64d0e665f40b2607879fcf2607764b2b70d672ddaa26d2da13049f" "aa87469691932ff791f966bffb885ecd97ebfa4dc4d42e479f3819ac4a3fbcaf" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "f5eb916f6bd4e743206913e6f28051249de8ccfd070eae47b5bde31ee813d55f" "0aa12caf6127772c1a38f7966de8258e7a0651fb6f7220d0bbb3a0232fba967f" "4f2ede02b3324c2f788f4e0bad77f7ebc1874eff7971d2a2c9b9724a50fb3f65" "870a63a25a2756074e53e5ee28f3f890332ddc21f9e87d583c5387285e882099" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "cedd3b4295ac0a41ef48376e16b4745c25fa8e7b4f706173083f16d5792bb379" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
