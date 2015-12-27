6dab1d01c4eb1d27e61e8587fbe575430a5c6e70
Peter Czibik <p.czibik@gmail.com>
2015-12-27 23:20:54 +0100

    Add remove trailing whitespace before save

--- settings/my-code.el
+++ settings/my-code.el
@@ -11,9 +11,9 @@
   (rainbow-delimiters-mode 1)
   (flycheck-mode 1)
   (diff-hl-mode 1)
-  ;; (whitespace-newline-mode 1)
   (linum-mode 1)
-  (show-paren-mode 1))
+  (show-paren-mode 1)
+  (add-hook 'before-save-hook 'delete-trailing-whitespace))
 
 (provide 'my-code)
 ;;; my-code.el ends here
